4deb142cd5ae5a31d35e2351a9d120eb7ddb9c13
Nguyễn Văn Quyết <67721370+quyetkaito68@users.noreply.github.com>
2021-09-06 14:06:46 +0700

    them

--- src/ltm_ex3/LTM_EX3.java
+++ src/ltm_ex3/LTM_EX3.java
@@ -15,7 +15,7 @@ public class LTM_EX3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.err.println("Hello world" + "hello work");
+        System.err.println("Hello world" + "hello work" + "wwww");
     }
     
 }
