8f395be936bd7f97e35553477197d2ffe7a6d7b6
Manh Hung <vuonghung2308@gmail.com>
2021-09-06 13:51:54 +0700

    first commit

--- src/ltm_ex3/LTM_EX3.java
+++ src/ltm_ex3/LTM_EX3.java
@@ -15,7 +15,7 @@ public class LTM_EX3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.err.println("Hello world");
     }
     
 }
