73cebacf323fcde1a34cb276c9baebbae9dea718
Manh Hung <vuonghung2308@gmail.com>
2021-09-06 22:26:36 +0700

    add some panel

--- src/ltm_ex3/LTM_EX3.java
+++ src/ltm_ex3/LTM_EX3.java
@@ -5,10 +5,37 @@
  */
 package ltm_ex3;
 
+import controller.TimeKeeperControl;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.UnsupportedLookAndFeelException;
+import model.Timekeeper;
+import view.MainFrame;
+
 public class LTM_EX3 {
 
-
     public static void main(String[] args) {
-       
+        try {
+            for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException 
+                | InstantiationException 
+                | IllegalAccessException 
+                | UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(MainFrame.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        MainFrame frame = new MainFrame();
+        TimeKeeperControl control = new TimeKeeperControl(frame.getTimeKeeperPanel(), new ArrayList<Timekeeper>());
+        java.awt.EventQueue.invokeLater(() -> {
+            frame.setVisible(true);
+        });
     }
 }
--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -14,13 +14,20 @@ public class MainFrame extends javax.swing.JFrame {
     /**
      * Creates new form MainFrame
      */
+    
+    private TimeKeeperPanel timeKeeperPanel;
     public MainFrame() {
         initComponents();
         setLocationRelativeTo(null);
+        timeKeeperPanel = new TimeKeeperPanel();
         jTabbedPane1.add("Phòng ban", new DepartmentPanel());
         jTabbedPane1.add("Nhân viên", new EmployeePanel());
-        jTabbedPane1.add("Thời gian làm", new TimeKeeperPanel());
+        jTabbedPane1.add("Thời gian làm", timeKeeperPanel);
         
+    }
+
+    public TimeKeeperPanel getTimeKeeperPanel() {
+        return timeKeeperPanel;
     }
 
     /**
@@ -49,42 +56,6 @@ public class MainFrame extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new MainFrame().setVisible(true);
-                
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTabbedPane jTabbedPane1;
