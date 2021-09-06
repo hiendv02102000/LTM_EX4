1307044b21a0e37396483be3158b43b7bd150216
Manh Hung <vuonghung2308@gmail.com>
2021-09-06 18:47:03 +0700

    remove unused lib

--- src/dbconnection/MySQLConnUtils.java
+++ src/dbconnection/MySQLConnUtils.java
@@ -12,32 +12,32 @@ package dbconnection;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
- 
-public class MySQLConnUtils {
- 
- // Kết nối vào MySQL.
- public static Connection getMySQLConnection() throws SQLException,
-         ClassNotFoundException {
-     String hostName = "20.198.248.201";
- 
-     String dbName = "contactdb";
-     String userName = "tmpuser";
-     String password = "tmpuser@123";
- 
-     return getMySQLConnection(hostName, dbName, userName, password);
- }
- 
- public static Connection getMySQLConnection(String hostName, String dbName,
-         String userName, String password) throws SQLException,
-         ClassNotFoundException {
 
-     Class.forName("com.mysql.jdbc.Driver");
- 
-     // Ví dụ: jdbc:mysql://localhost:3306/simplehr
-     String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName+"?zeroDateTimeBehavior=convertToNull";
- 
-     Connection conn = DriverManager.getConnection(connectionURL, userName,
-             password);
-     return conn;
- }
+public class MySQLConnUtils {
+
+    // Kết nối vào MySQL.
+    public static Connection getMySQLConnection()
+            throws SQLException, ClassNotFoundException {
+        String hostName = "20.198.248.201";
+        String dbName = "contactdb";
+        String userName = "tmpuser";
+        String password = "tmpuser@123";
+        
+        return getMySQLConnection(hostName, dbName, userName, password);
+    }
+
+    public static Connection getMySQLConnection(
+            String hostName, String dbName,
+            String userName, String password
+    ) throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        
+        String connectionURL = "jdbc:mysql://" + hostName + ":3306/"
+                + dbName + "?zeroDateTimeBehavior=convertToNull";
+        
+        Connection conn = DriverManager.getConnection(
+                connectionURL, userName, password
+        );
+        return conn;
+    }
 }
--- src/ltm_ex3/LTM_EX3.java
+++ src/ltm_ex3/LTM_EX3.java
@@ -5,8 +5,9 @@
  */
 package ltm_ex3;
 
+import dbconnection.MySQLConnUtils;
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -16,31 +17,30 @@ import java.sql.Statement;
  */
 public class LTM_EX3 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws SQLException {
-        String host = "20.198.248.201";
-        String dbName = "contactdb";
-        int port = 3306;
-        String mysqlURL = "jdbc:mysql://" + host + ":3306/" + dbName;
-
-        String username = "tmpuser";
-        String password = "tmpuser@123";
-
-        String dbClass = "com.mysql.jdbc.Driver";
-        try {
-            Class.forName(dbClass);
-        } catch (ClassNotFoundException cnfe) {
-            System.out.println("Error loading driver: " + cnfe);
-        }
-        Connection connection = (Connection) DriverManager.getConnection(mysqlURL, username, password);
-
-        Statement statement = connection.createStatement();
-        connection.close();
-        System.err.println("Hello world" + "hello work" + "wwww");
-
-        System.out.println("hiii");
+    public static Connection getMyConnection() throws SQLException,
+            ClassNotFoundException {
+        return MySQLConnUtils.getMySQLConnection();
     }
 
+    /**
+     * @param args the command line arguments
+     * @throws java.sql.SQLException
+     * @throws java.lang.ClassNotFoundException
+     */
+    public static void main(String[] args)
+            throws SQLException, ClassNotFoundException {
+
+        System.out.println("Get connection ... ");
+        Connection conn = LTM_EX3.getMyConnection();
+
+        Statement state = conn.createStatement();
+        ResultSet result = state.executeQuery("SELECT * FROM contactdb.EMPLOYEE");
+
+        while (result.next()) {
+            System.out.println(result.getString(1) + " "
+                    + result.getString(2) + " "
+                    + result.getString(3));
+        }
+        System.out.println("Done!");
+    }
 }
