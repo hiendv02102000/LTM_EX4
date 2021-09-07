f2eef78c59d12ef16ad738f66ae160eb34f0a92c
Manh Hung <vuonghung2308@gmail.com>
2021-09-07 11:18:35 +0700

    remove unused functions

--- src/controller/utils/ConnectionUtils.java
+++ src/controller/utils/ConnectionUtils.java
@@ -7,25 +7,6 @@ public class ConnectionUtils {
 
     public static Connection getMyConnection() throws SQLException,
             ClassNotFoundException {
-        // Sử dụng Mysql.
-        // Bạn có thể thay thế bởi Database nào đó.
         return MySQLConnUtils.getMySQLConnection();
     }
-
-    //
-    // Test Connection ...
-    //
-    public static void main(String[] args) throws SQLException,
-            ClassNotFoundException {
-
-        System.out.println("Get connection ... ");
-
-        // Lấy ra đối tượng Connection kết nối vào database.
-        Connection conn = ConnectionUtils.getMyConnection();
-
-        System.out.println("Get connection " + conn);
-
-        System.out.println("Done!");
-    }
-
 }
